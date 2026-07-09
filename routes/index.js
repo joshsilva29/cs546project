21e2de1f372efd01cb08d33066d978588b1e79cd
joshsilva29 <106221807+joshsilva29@users.noreply.github.com>
2026-07-09 19:31:13 -0400

    added routes to nycClosures.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ import nycClosuresRoutes from "./nycClosures.js";
 const configureRoutes = (app) => {
     app.use('/users', usersRoutes);
     // app.use('/closures', closuresRoutes);
-    app.use('/nycOpenData', nycClosuresRoutes);
+    app.use('/nycClosures', nycClosuresRoutes);
 
     app.use('/{*splat}', (req, res) => {
         return res.status(404).json({error: 'Not found'});
--- routes/nycClosures.js
+++ routes/nycClosures.js
@@ -1,22 +1,27 @@
 import {Router} from 'express';
 const router = Router();
 
-router
-  .route('/')
-  .get(async (req, res) => {
-    return res.send('GET request to http://localhost:3000/nycClosures');
-  })
-  .post(async (req, res) => {
-    return res.send('POST request to http://localhost:3000/nycClosures');
-  })
-  .delete(async (req, res) => {
-    return res.send('DELETE request to http://localhost:3000/nycClosures');
-  })
-  .put(async (req, res) => {
-    return res.send('PUT request to http://localhost:3000/nycClosures');
-  })
-  .patch(async (req, res) => {
-    return res.send('PATCH request to http://localhost:3000/nycClosures');
-  });
+//get duration of closure
+router.get('/duration/:id', (req, res) => {
+  return res.send(`GET request to http://localhost:3000/duration/${req.params.id}`);
+});
+
+//get history of closures on this street (street sent in body)
+//
+//not sure if this can be done with nyc open data
+router.post('/history', (req, res) => {
+  return res.send(`POST request to http://localhost:3000/history`);
+});
+
+//get closures near a current location (street sent in body)
+router.post('/nearYou', (req, res) => {
+  return res.send(`POST request to http://localhost:3000/nearYou`);
+});
+
+//get closures in or around a certain street (street sent in body)
+router.post('/closureSearch', (req, res) => {
+  return res.send(`POST request to http://localhost:3000/closureSearch`);
+});
+
 
 export default router;
