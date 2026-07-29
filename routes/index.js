734b2cab07a349ee2a66beea76ab85f7438e6de2
joshsilva29 <106221807+joshsilva29@users.noreply.github.com>
2026-07-29 19:28:14 -0400

    added handlebars functionality and navigation.js routes file

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ import durationRoutes       from "./closureDuration.js";
 import historyRoutes        from "./closureHistory.js";
 import nearYouRoutes        from "./closureNearYou.js";
 import searchRoutes         from "./closureSearch.js";
+import navigationRoutes     from "./navigation.js";
 
 const configureRoutes = (app) => {
     app.use('/users',       usersRoutes);
     app.use('/closures',    closuresRoutes);
+    app.use(navigationRoutes);
     app.use(reportClosureRoutes);
     app.use(durationRoutes);
     app.use(historyRoutes);
