471dd1b948f707ea7d2a767bfb09ca2c2794a679
joshsilva29 <106221807+joshsilva29@users.noreply.github.com>
2026-07-30 04:59:21 -0400

    added nyc open data closure /closureSearch?street={street} functionality to frontend. can also toggle between all, user-reported-closures, and nyc open data

--- public/js/search.js
+++ public/js/search.js
@@ -1,10 +1,29 @@
 
+let searchOption = "all";
+
 $("#search_form").submit(async (event) => {
     event.preventDefault();
     
     let street = $("#search_input").val().trim();
     if (!street) alert("You need to provide a street name");
 
+    $("#results").empty();
+
+    if (searchOption === "all" || searchOption === "user-reported") {
+        await closureSearch(street);
+    }
+
+    if (searchOption === "all" || searchOption === "nyc") {
+        await nycClosureSearch(street);
+    }
+
+    $('#search_form').trigger('reset');
+    $('#search_input').focus();
+});
+
+//-----------------------------------------------------
+
+async function closureSearch(street) {
     let response, closureResults;
 
     //THIS IS FOR THE MONGODB CLOSURES DATABASE
@@ -17,22 +36,76 @@ $("#search_form").submit(async (event) => {
 
     closureResults = await response.json();
 
-    $("#results").empty();
+    console.log(closureResults);
 
-    for(let closure of closureResults) {
-        let id = closure._id;
-        let onStreet = closure.on_street_name;
-        let fromStreet = closure.from_street_name;
-        let toStreet = closure.to_street_name;
-        let closureElement = `
-            <div id=${id} class="closure-element">
-                <p>${onStreet}</p>
-                <p>From ${onStreet} to ${toStreet}</p>
-            </div>
-        `;
-        $("#results").append(closureElement);
+    if (closureResults.length === 0) {
+        let noResultsElement = `<div>No user closures were found for ${street}</div>`;
+        $("#results").append(noResultsElement);
+    } else {
+        for(let closure of closureResults) {
+            let id = closure._id;
+            let onStreet = closure.on_street_name;
+            let fromStreet = closure.from_street_name;
+            let toStreet = closure.to_street_name;
+            let closureElement = `
+                <div id=${id} class="closure-element">
+                    <p>${onStreet}</p>
+                    <p>From ${fromStreet} to ${toStreet}</p>
+                </div>
+            `;
+            $("#results").append(closureElement);
+        }
+    }
+}
+
+async function nycClosureSearch(street) {
+    let response, closureResults;
+
+    //THIS IS FOR NYC OPEN DATA
+
+    try {
+        response = await fetch(`/closureSearch?street=${street}`);
+    } catch (e) {
+        alert("There was an error fetching the results");
     }
 
-    $('#search_form').trigger('reset');
-    $('#search_input').focus();
-});
+    try {
+        closureResults = await response.json();
+        console.log(closureResults);
+        for(let closure of closureResults.results) {
+            let onStreet = closure.street;
+            let toStreet = closure.toStreet || "";
+            let fromStreet = toStreet ? `${closure.fromStreet} to` : closure.fromStreet;
+            let closureElement = `
+                <div class="closure-element">
+                    <p>${onStreet}</p>
+                    <p>From ${fromStreet} ${toStreet}</p>
+                </div>
+            `;
+            $("#results").append(closureElement);
+        }
+    } catch (e) {
+        let noResultsElement = `<div>No NYC closures were found for ${street}</div>`;
+        $("#results").append(noResultsElement);
+    }
+    
+}
+
+async function toggleOption(option) {
+    if (option === "all") {
+        $("#all").addClass("selected-class");
+        $("#user-reported").removeClass("selected-class");
+        $("#nyc").removeClass("selected-class");
+    } else if (option === "user-reported") {
+        $("#all").removeClass("selected-class");
+        $("#user-reported").addClass("selected-class");
+        $("#nyc").removeClass("selected-class");
+    } else {
+        $("#all").removeClass("selected-class");
+        $("#user-reported").removeClass("selected-class");
+        $("#nyc").addClass("selected-class");
+    }
+    searchOption = option;
+
+    console.log(searchOption);
+}
