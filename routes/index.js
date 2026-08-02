cb979c6d157eb0aec8d98850323d07f7543a87c4
Aaron <aaronshieh3@gmail.com>
2026-08-02 13:56:20 -0400

    all profile/home pages placed in home.handlebars layout

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ import historyRoutes        from "./closureHistory.js";
 import nearYouRoutes        from "./closureNearYou.js";
 import searchRoutes         from "./closureSearch.js";
 import navigationRoutes     from "./navigation.js";
+import { Timestamp } from "mongodb";
 
 const configureRoutes = (app) => {
     app.use('/users',       usersRoutes);
@@ -18,7 +19,11 @@ const configureRoutes = (app) => {
     app.use(searchRoutes);
 
     app.use('/{*splat}', (req, res) => {
-        return res.status(404).json({ error: 'Not found' });
+        return res.status(404).render('error', {
+            layout: 'home',
+            error: 'Page Not Found',
+            title: 'Error'
+        })
     });
 }
 
--- routes/navigation.js
+++ routes/navigation.js
@@ -8,8 +8,20 @@ router.get('/search', async (req, res) => {
     return res.render("search");
 });
 
-router.get('/home', async (req, res) => {
-    return res.render("home");
+router.get('/notifications', async (req, res) => {
+    return res.render("notifications", {
+        layout: 'home',
+        css: 'notifications',
+        title: 'Notifications'
+    });
+});
+
+router.get('/savedStreets', async (req, res) => {
+    return res.render("savedStreets", {
+        layout: 'home',
+        css: 'savedStreets',
+        title: 'Saved Streets'
+    });
 });
 
 export default router;
